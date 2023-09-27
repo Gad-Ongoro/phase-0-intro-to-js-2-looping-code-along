569554a152ea2e0b9883b0aaa0090e68bc5d17f8
Gad-Ongoro <gadongoro1@gmail.com>
2023-09-27 17:40:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(stringNames , eventName) {
+  let logMessage = [];
+    for (let i = 0; i < stringNames.length; i++) {
+      logMessage.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+  return logMessage;
+  }
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--){
+    console.log(i);
+  }
+}
